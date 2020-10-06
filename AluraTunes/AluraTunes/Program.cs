429a995d375e3ac3770a45838617db511fa0e315
gilvan <g.r.almeida@live.com>
2020-10-06 10:26:49 -0300

    Linq com joins

--- AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/Program.cs
@@ -8,7 +8,44 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
-            IntroducaoAoLinqSelectFromWhere();
+            LinqComJoins();
+        }
+
+        private static void LinqComJoins()
+        {
+            // Listar músicas
+
+            var generos = new List<Genero>
+            {
+                new Genero { Id = 1, Nome = "Rock" },
+                new Genero { Id = 2, Nome = "Reggae" },
+                new Genero { Id = 3, Nome = "Rock Progressivo" },
+                new Genero { Id = 4, Nome = "Punk Rock" },
+                new Genero { Id = 5, Nome = "Clássica" }
+            };
+
+            var musicas = new List<Musica>
+            {
+                new Musica { Id = 1, Nome = "Sweet Child O' Mine", GeneroId = 1 },
+                new Musica { Id = 2, Nome = "I Shot The Sheriff", GeneroId = 2 },
+                new Musica { Id = 3, Nome = "Danúbio Azul", GeneroId = 5 }
+            };
+
+            var musicaQuery = from musica in musicas
+                              join genero in generos on musica.GeneroId equals genero.Id
+                              select new
+                              {
+                                  musica.Id,
+                                  musica.Nome,
+                                  genero
+                              };
+
+            foreach (var musica in musicaQuery)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", musica.Id, musica.Nome, musica.genero.Nome);
+            }
+
+            Console.ReadKey();
         }
 
         private static void IntroducaoAoLinqSelectFromWhere()
@@ -53,5 +90,12 @@ namespace AluraTunes
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+    }
+
+    class Musica
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int GeneroId { get; set; }
     }
 }
